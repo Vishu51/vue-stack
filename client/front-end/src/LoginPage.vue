cf0af58f14967f3acc2684d4837476c3d3ddf266
Vishu51 <vishal.gupta@diatoz.com>
2023-04-14 17:20:07 +0530

    front-end changes.

---
+++ client/front-end/src/LoginPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-container>
+      <v-card class="elevation-12">
+        <v-card-title>Login</v-card-title>
+        <v-card-text>
+          <v-form @submit.prevent="login">
+            <v-text-field
+              label="Username"
+              v-model="username"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Password"
+              v-model="password"
+              type="password"
+              required
+            ></v-text-field>
+            <v-btn color="primary" type="submit">Login</v-btn>
+          </v-form>
+          <v-alert v-if="error" type="error">{{ error }}</v-alert>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      username: '',
+      password: '',
+      error: ''
+    }
+  },
+  methods: {
+    login() {
+      if (this.username === 'admin' && this.password === 'password') {
+        alert('Login successful!');
+      } else {
+        this.error = 'Incorrect username or password.';
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- client/front-end/vue.config.js
+++ client/front-end/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: ['vuetify']
 })
+
+config => {
+  config.module
+    .rule('eslint')
+    .use('eslint-loader')
+    .tap(options => {
+      options.fix = true
+      return options
+    })
+  config.plugin('html').tap(args => {
+    args[0].title = 'Dashboard'
+    return args
+  })
+}
